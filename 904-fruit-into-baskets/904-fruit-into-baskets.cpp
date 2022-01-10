adebfac1101e47944ab4dc3cbfeb721786227732
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-01-10 21:37:01 +0530

    Time: 213 ms (20.79%), Space: 61.4 MB (49.47%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        int ans=0;
+        unordered_map<int,int>mpp;
+        int rel=0;
+        int distinct_count=0;
+        for(int i=0;i<n;i++){
+           if(mpp.find(fruits[i])!=mpp.end()){
+               mpp[fruits[i]]++;
+           }
+            else{
+                mpp[fruits[i]]=1;
+                distinct_count++;
+            }
+            
+            if(distinct_count>2){
+                
+                while(distinct_count>2 && rel<=i){
+                    mpp[fruits[rel]]--;
+                    if(mpp[fruits[rel]]==0){
+                        mpp.erase(fruits[rel]);
+                        distinct_count--;
+                    }
+                    rel++;
+                }
+            }
+            ans=max(ans,i-rel+1);
+        }
+        return ans;
+    }
+};
