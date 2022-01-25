c2ca243724cf8b2597541349d46469e90e88f8e4
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-01-25 16:17:01 +0530

    Added solution - LeetHub

---
+++ Reach the Nth point - GFG/reach-the-nth-point.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+	    
+		int nthPoint(int n)
+		{
+            vector<long long int>dp(n+1,-1);
+            dp[0]=1;
+            dp[1]=1;
+		    for(int i=2;i<=n;i++){
+		        dp[i]=dp[i-1]+dp[i-2]%1000000007;
+		    }
+		    return dp[n]%1000000007;
+		}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthPoint(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
