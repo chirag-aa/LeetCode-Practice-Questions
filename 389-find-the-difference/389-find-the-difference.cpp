4424c41fb6dbf2d63bbafe8bc835c013289a9134
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-02-07 10:56:22 +0530

    Time: 4 ms (67.41%), Space: 6.6 MB (67.00%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mpp;
+        char ans;
+        for(int i=0;i<t.length();i++){
+            mpp[t[i]]++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(mpp.find(s[i])!=mpp.end()){
+                mpp[s[i]]--;
+                if(mpp[s[i]]==0){
+                    mpp.erase(s[i]);
+                }
+            }
+        }
+        for(auto &it:mpp){
+            return ans=it.first;
+        }
+        return ans;
+    }
+};
