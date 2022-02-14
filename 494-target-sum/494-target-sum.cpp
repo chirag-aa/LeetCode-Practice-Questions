e75e0ce0a22aa56d814e4c8c981f3b133ed25d75
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-02-14 12:21:12 +0530

    Time: 1003 ms (18.13%), Space: 89.8 MB (5.01%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int targetSum(vector<int>& nums,int target,int idx,int n){
+    int targetSum(vector<int>& nums,int target,int idx,int n,unordered_map<string,int>& mpp){
         if(idx==n && target!=0){
             return 0;
         }
         if(idx==n && target==0){
             return 1;
         }
-        int add=targetSum(nums,target-nums[idx],idx+1,n);
-        int sub=targetSum(nums,target+nums[idx],idx+1,n);
+        string makeKey= to_string(idx)+ "_" + to_string(target);
+        if(mpp.find(makeKey)!=mpp.end()){
+            return mpp[makeKey];
+        }
+        int add=targetSum(nums,target-nums[idx],idx+1,n,mpp);
+        int sub=targetSum(nums,target+nums[idx],idx+1,n,mpp);
         
-        return add+sub;
+        return mpp[makeKey]= add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int idx=0;
-        int ans= targetSum(nums,target,idx,n);
+        unordered_map<string,int>mpp;
+        int ans= targetSum(nums,target,idx,n,mpp);
         return ans;
     }
 };
