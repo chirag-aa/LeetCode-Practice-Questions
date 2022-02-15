96d7360d3767dd700852b1a9599f5140cff39c15
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-02-15 13:44:55 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -9,29 +9,44 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int maxProfit(int w,int wt[],int val[],int n,int idx,unordered_map<string,int>& mpp){
-        if(idx==n || w<=0){
-            return 0;
-        }
-        string makeKey= to_string(idx)+ "_" +to_string(w);
-        if(mpp.find(makeKey)!=mpp.end()){
-            return mpp[makeKey];
-        }
-        int include=INT_MIN;
+    // int maxProfit(int w,int wt[],int val[],int n,int idx,unordered_map<string,int>& mpp){
+    //     if(idx==n || w<=0){
+    //         return 0;
+    //     }
+    //     string makeKey= to_string(idx)+ "_" +to_string(w);
+    //     if(mpp.find(makeKey)!=mpp.end()){
+    //         return mpp[makeKey];
+    //     }
+    //     int include=INT_MIN;
         
-        if(w-wt[idx]>=0){
-            include=maxProfit(w-wt[idx],wt,val,n,idx+1,mpp)+val[idx];
-        }
-        int notInclude=maxProfit(w,wt,val,n,idx+1,mpp)+0;
+    //     if(w-wt[idx]>=0){
+    //         include=maxProfit(w-wt[idx],wt,val,n,idx+1,mpp)+val[idx];
+    //     }
+    //     int notInclude=maxProfit(w,wt,val,n,idx+1,mpp)+0;
         
-        return mpp[makeKey]=max(include,notInclude);
-    }
+    //     return mpp[makeKey]=max(include,notInclude);
+    // }
     int knapSack(int w, int wt[], int val[], int n) 
     { 
         int idx=0;
-        unordered_map<string,int>mpp;
-        int ans=maxProfit(w,wt,val,n,idx,mpp);
-        return ans;
+     //   unordered_map<string,int>mpp;
+      //  int ans=maxProfit(w,wt,val,n,idx,mpp);
+        vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+        for(int i=1;i<=n;i++){
+              int include=0;
+                int notinclude=0;
+            for(int j=1;j<=w;j++){
+              
+                if(j-wt[i-1]>=0){
+                    include= val[i-1]+ dp[i-1][j-wt[i-1]];
+                }
+                
+                    notinclude=0+ dp[i-1][j];
+                
+                dp[i][j]=max(include,notinclude);
+            }
+        }
+        return dp[n][w];
     }
 };
 
