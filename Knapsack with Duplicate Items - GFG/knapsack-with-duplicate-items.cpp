e67fbb0bc5380f7ecf430b205a48fa7e3b42dc26
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-02-17 16:33:00 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+   int maxProfit(int n,int w,int val[],int wt[],vector<vector<int>>& dp,int idx){
+       if(w==0){
+           return 0;
+       }
+       if(idx>=n){
+           return 0;
+       }
+       if(dp[idx][w]!=-1){
+           return dp[idx][w];
+       }
+       int include=0;
+       if(w-wt[idx]>=0){
+           include=maxProfit(n,w-wt[idx],val,wt,dp,idx)+val[idx];
+       }
+       int notInclude=0+maxProfit(n,w,val,wt,dp,idx+1);
+       
+       return dp[idx][w]=max(include,notInclude);
+   }
+    int knapSack(int n, int w, int val[], int wt[])
+    {
+       int idx=0;
+       vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+       for(int i=0;i<=n;i++){
+           for(int j=0;j<=w;j++){
+               dp[i][j]= -1;
+           }
+       }
+       int ans=maxProfit(n,w,val,wt,dp,idx);
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, W;
+        cin>>N>>W;
+        int val[N], wt[N];
+        for(int i = 0;i < N;i++)
+            cin>>val[i];
+        for(int i = 0;i < N;i++)
+            cin>>wt[i];
+        
+        Solution ob;
+        cout<<ob.knapSack(N, W, val, wt)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
