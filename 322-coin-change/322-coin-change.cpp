f7f2c59ab82ca181faffa03e1214dc31716789ef
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2022-02-18 18:02:31 +0530

    Time: 92 ms (66.65%), Space: 10.6 MB (83.27%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int minCoins(vector<int>& coins, int amount, int idx,int n, vector<vector<int>>& dp){
-        if(amount==0){
-            return 0;
-        }
-        if(idx>=n){
-            return INT_MAX;
-        }
-        if(dp[idx][amount]!=-1){
-            return dp[idx][amount];
-        }
-        int include =10000;
-        if(amount-coins[idx]>=0){
-            include= minCoins(coins,amount-coins[idx],idx,n,dp)+1;
-        }
-        int notinclude=INT_MAX;
-        notinclude=minCoins(coins,amount,idx+1,n,dp)+0;
-        
-        return dp[idx][amount]=min(include,notinclude);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        int idx=0;
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans=minCoins(coins,amount,idx,n,dp);
-        if(ans>=10000){
+        int dp[n+1][amount+1];
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(i==0){
+                    dp[i][j]= 10000;
+                }
+                if(j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+            for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                int take= 10000;
+                if(j-coins[i-1]>=0){
+                    take=dp[i][j-coins[i-1]]+1;
+                }
+                int notTake=dp[i-1][j];
+                dp[i][j]=min(take,notTake);
+            }
+        }
+        if(dp[n][amount]==10000){
             return -1;
         }
-        return ans;
+        return dp[n][amount];
     }
 };
