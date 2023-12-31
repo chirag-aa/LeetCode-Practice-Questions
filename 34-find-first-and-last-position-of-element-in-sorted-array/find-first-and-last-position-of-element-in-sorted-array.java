77709394151645e438fd66f6696ea9c645a674f8
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2023-12-31 07:13:18 -0800

    Time: 0 ms (100.00%) | Memory: 45.4 MB (16.60%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,57 @@
 class Solution {
+    public int findFirstAccurance(int []nums,int target){
+        int n = nums.length;
+
+        int firstAcc = -1;
+        int start=0;
+        int end = n-1;
+
+        while(start<=end){
+          int mid = (start+end)/2;
+          if(nums[mid]==target){
+              firstAcc = mid;
+              end = mid -1;
+          }
+          else if(nums[mid]<target){
+              start = mid + 1;
+          }
+          else {
+              end = mid - 1;
+          }
+        }
+        return firstAcc;
+    }
+    public int findLastAccurance(int []nums,int target){
+        int n = nums.length;
+
+        int lastAcc = -1;
+        int start=0;
+        int end = n-1;
+
+        while(start<=end){
+          int mid = (start+end)/2;
+          if(nums[mid]==target){
+              lastAcc = mid;
+              start = mid + 1;
+          }
+          else if(nums[mid]<target){
+              start = mid + 1;
+          }
+          else {
+              end = mid - 1;
+          }
+        }
+        return lastAcc;
+    }
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int arr[] = new int[2];
-         arr[0] = -1;
-        arr[1] = -1;
+        int ans[] = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
 
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                arr[0] = i;
-                break;
-            }
-        }
-        for(int j = n-1;j>=0;j--){
-            if(nums[j]==target){
-                arr[1]=j;
-                break;
-            }
-        }
-        return arr;
+        ans[0] = findFirstAccurance(nums,target);
+        ans[1] = findLastAccurance(nums,target);
+
+        return ans;
     }
 }
