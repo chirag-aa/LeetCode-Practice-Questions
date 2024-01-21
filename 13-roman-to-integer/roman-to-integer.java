da7411ed7ed2dacc19dcc4169aee83ddb45cdfc1
Chirag Ambwani <69840969+chirag-aa@users.noreply.github.com>
2024-01-20 23:11:29 -0800

    Time: 5 ms (59.54%) | Memory: 44.8 MB (28.14%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -15,32 +15,32 @@ class Solution {
             if(i+1<n && s.charAt(i+1)=='V' && s.charAt(i)=='I'){
                 ans = ans + 4;
                 i++;
-                continue;
+            
             }
-            if(i+1<n && s.charAt(i+1)=='X' && s.charAt(i)=='I'){
+            else if(i+1<n && s.charAt(i+1)=='X' && s.charAt(i)=='I'){
                 ans = ans + 9;
                 i++;
-                continue;
+                
             }
-            if(i+1<n && s.charAt(i+1)=='L' && s.charAt(i)=='X'){
+            else if(i+1<n && s.charAt(i+1)=='L' && s.charAt(i)=='X'){
                 ans = ans + 40;
                 i++;
-                continue;
+                
             }
-            if(i+1<n && s.charAt(i+1)=='C' && s.charAt(i)=='X'){
+            else if(i+1<n && s.charAt(i+1)=='C' && s.charAt(i)=='X'){
                 ans = ans + 90;
                 i++;
-                continue;
+                
             }
-            if(i+1<n && s.charAt(i+1)=='D' && s.charAt(i)=='C'){
+            else if(i+1<n && s.charAt(i+1)=='D' && s.charAt(i)=='C'){
                 ans = ans + 400;
                 i++;
-                continue;
+                
             }
-            if(i+1<n && s.charAt(i+1)=='M' && s.charAt(i)=='C'){
+            else if(i+1<n && s.charAt(i+1)=='M' && s.charAt(i)=='C'){
                 ans = ans + 900;
                 i++;
-                continue;
+            
             }
             else {
                 char ch = s.charAt(i);
